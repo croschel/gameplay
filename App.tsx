d4f88207d89a29c6a99ef4952886ab6c9b08e0da
Caique Roschel <croschel000@gmail.com>
2021-06-21 19:07:04 -0300

    root App

--- App.tsx
+++ App.tsx
@@ -1,12 +1,16 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React from "react";
+import SignIn from "./src/SignIn";
+import { StyleSheet, Text, View, StatusBar } from "react-native";
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <StatusBar
+        barStyle="light-content"
+        backgroundColor="transparent"
+        translucent
+      />
+      <SignIn />
     </View>
   );
 }
@@ -14,8 +18,5 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
   },
 });
