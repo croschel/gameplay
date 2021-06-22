6763d0072a416ed7b7d5e29105902729248eb8bb
Caique Roschel <croschel000@gmail.com>
2021-06-22 20:13:49 -0300

    new fonts and loading expo core component

--- App.tsx
+++ App.tsx
@@ -1,8 +1,22 @@
 import React from "react";
-import SignIn from "./src/SignIn";
-import { StyleSheet, Text, View, StatusBar } from "react-native";
+import SignIn from "./src/screens/SignIn";
+import { StyleSheet, View, StatusBar } from "react-native";
+import { useFonts } from "expo-font";
+import { Inter_400Regular, Inter_500Medium } from '@expo-google-fonts/inter';
+import { Rajdhani_500Medium, Rajdhani_700Bold } from '@expo-google-fonts/rajdhani';
+import AppLoading from 'expo-app-loading';
 
 export default function App() {
+ const [fontsLoaded] = useFonts({
+    Inter_400Regular,
+    Inter_500Medium,
+    Rajdhani_500Medium,
+    Rajdhani_700Bold
+  });
+
+  if(!fontsLoaded){
+    return <AppLoading/>;
+  }
   return (
     <View style={styles.container}>
       <StatusBar
