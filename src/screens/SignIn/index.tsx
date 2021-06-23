559180b16d01decfad2397256376215184876550
Caique Roschel <croschel000@gmail.com>
2021-06-22 23:36:02 -0300

    SignIn updates

--- src/screens/SignIn/index.tsx
+++ src/screens/SignIn/index.tsx
@@ -2,9 +2,18 @@ import React from "react";
 import { View, Image, Text } from "react-native";
 import ButtonIcon from "~/components/ButtonIcon";
 import IllustrationImg from "~/assets/illustration.png";
+import DiscordImg from "~/assets/discord.png";
+
 import { styles } from "./styles";
+import { useNavigation } from "@react-navigation/native";
 
 const SignIn: React.FC = () => {
+  const navigation = useNavigation();
+
+  const handleSignIn = () => {
+    navigation.navigate("Home");
+  };
+
   return (
     <View style={styles.container}>
       <Image
@@ -21,7 +30,11 @@ const SignIn: React.FC = () => {
           Crie grupos para jogar seus games {`\n`}
           favoritos com seus amigos
         </Text>
-        <ButtonIcon />
+        <ButtonIcon
+          icon={DiscordImg}
+          title="Entrar com Discord"
+          onPress={handleSignIn}
+        />
       </View>
     </View>
   );
