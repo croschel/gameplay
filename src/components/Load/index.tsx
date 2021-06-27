1f4a3c89c2ad4c77eb14d94cf42e4c8de8fd60fe
Caique Roschel <croschel000@gmail.com>
2021-06-27 19:21:23 -0300

    Load components with ActivityIndicator

---
+++ src/components/Load/index.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+import { View, ActivityIndicator } from "react-native";
+import { colors } from "~/global/styles/theme";
+
+import { styles } from "./styles";
+
+export function Load() {
+  return (
+    <View style={styles.container}>
+      <ActivityIndicator size="large" color={colors.primary} />
+    </View>
+  );
+}
