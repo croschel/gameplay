a8f4b4906b0c02000eccb1a9a32ca7984f81d4d2
Caique Roschel <croschel000@gmail.com>
2021-06-27 11:42:22 -0300

    separate Mocks

---
+++ src/mocks/members.ts
@@ -0,0 +1,38 @@
+export const members = [
+  {
+    id: "1",
+    username: "Caique",
+    avatar: "https://github.com/croschel.png",
+    status: "online",
+  },
+  {
+    id: "2",
+    username: "Caique",
+    avatar: "http://github.com/croschel.png",
+    status: "offline",
+  },
+  {
+    id: "3",
+    username: "Caique",
+    avatar: "http://github.com/croschel.png",
+    status: "offline",
+  },
+  {
+    id: "4",
+    username: "Caique",
+    avatar: "http://github.com/croschel.png",
+    status: "offline",
+  },
+  {
+    id: "5",
+    username: "Caique",
+    avatar: "http://github.com/croschel.png",
+    status: "offline",
+  },
+  {
+    id: "6",
+    username: "Caique",
+    avatar: "http://github.com/croschel.png",
+    status: "offline",
+  },
+];
