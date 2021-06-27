5aa80b8666d40fa19055fca08c060ae4c8877389
Caique Roschel <croschel000@gmail.com>
2021-06-26 22:28:29 -0300

    guild component

---
+++ src/components/Guild/index.tsx
@@ -0,0 +1,41 @@
+import React from "react";
+import {
+  View,
+  Text,
+  TouchableOpacity,
+  TouchableOpacityProps,
+} from "react-native";
+import GuildIcon from "../GuildIcon";
+import { Feather } from "@expo/vector-icons";
+import { styles } from "./styles";
+import { colors } from "~/global/styles/theme";
+
+export type GuildProps = {
+  id: string;
+  name: string;
+  icon: string | null;
+  owner: boolean;
+};
+
+type Props = TouchableOpacityProps & {
+  data: GuildProps;
+};
+
+const Guild = ({ data, ...rest }: Props) => {
+  return (
+    <TouchableOpacity style={styles.container} activeOpacity={0.7} {...rest}>
+      <GuildIcon />
+      <View style={styles.content}>
+        <View>
+          <Text style={styles.title}>{data.name}</Text>
+          <Text style={styles.type}>
+            {data.owner ? "Administrador" : "Convidado"}
+          </Text>
+        </View>
+      </View>
+      <Feather name="chevron-right" size={24} color={colors.heading} />
+    </TouchableOpacity>
+  );
+};
+
+export default Guild;
