af18ee7a41daa8592e3728ac0e778916c1682f8c
Caique Roschel <croschel000@gmail.com>
2021-06-27 19:42:38 -0300

    creating appointments

--- src/screens/AppointmentCreate/index.tsx
+++ src/screens/AppointmentCreate/index.tsx
@@ -6,6 +6,7 @@ import {
   KeyboardAvoidingView,
   Platform,
 } from "react-native";
+import uuid from "react-native-uuid";
 import { Feather } from "@expo/vector-icons";
 import { RectButton } from "react-native-gesture-handler";
 import Button from "~/components/Button";
@@ -19,11 +20,22 @@ import GuildIcon from "~/components/GuildIcon";
 import { styles } from "./styles";
 import { colors } from "~/global/styles/theme";
 import { GuildProps } from "~/components/Guild";
+import AsyncStorage from "@react-native-async-storage/async-storage";
+import { COLLECTION_APPOINTMENTS } from "~/configs/database";
+import { useNavigation } from "@react-navigation/core";
 
 const AppointmentCreate: React.FC = () => {
+  const navigation = useNavigation();
+
   const [category, setCategory] = useState("");
   const [openGuildsModal, setOpenGuildsModal] = useState(false);
   const [guild, setGuild] = useState<GuildProps>({} as GuildProps);
+
+  const [day, setDay] = useState("");
+  const [month, setMonth] = useState("");
+  const [hour, setHour] = useState("");
+  const [minute, setMinute] = useState("");
+  const [description, setDescription] = useState("");
 
   const handleOpenGuilds = () => {
     setOpenGuildsModal(true);
@@ -40,6 +52,23 @@ const AppointmentCreate: React.FC = () => {
 
   const handleCategorySelect = (categoryId: string) => {
     setCategory(categoryId);
+  };
+
+  const handleSave = async () => {
+    const newAppointment = {
+      id: uuid.v4(),
+      guild,
+      category,
+      date: `${day}/${month} às ${hour}:${minute}`,
+      description,
+    };
+    const storage = await AsyncStorage.getItem(COLLECTION_APPOINTMENTS);
+    const appointments = storage ? JSON.parse(storage) : [];
+    await AsyncStorage.setItem(
+      COLLECTION_APPOINTMENTS,
+      JSON.stringify([...appointments, newAppointment])
+    );
+    navigation.navigate("Home");
   };
 
   return (
@@ -85,9 +114,9 @@ const AppointmentCreate: React.FC = () => {
                 Dia e mês
               </Text>
               <View style={styles.column}>
-                <SmallInput maxLength={2} />
+                <SmallInput maxLength={2} onChangeText={setDay} />
                 <Text style={styles.divider}>/</Text>
-                <SmallInput maxLength={2} />
+                <SmallInput maxLength={2} onChangeText={setMonth} />
               </View>
             </View>
             <View>
@@ -95,9 +124,9 @@ const AppointmentCreate: React.FC = () => {
                 Hora e minuto
               </Text>
               <View style={styles.column}>
-                <SmallInput maxLength={2} />
+                <SmallInput maxLength={2} onChangeText={setHour} />
                 <Text style={styles.divider}>:</Text>
-                <SmallInput maxLength={2} />
+                <SmallInput maxLength={2} onChangeText={setMinute} />
               </View>
             </View>
           </View>
@@ -111,9 +140,10 @@ const AppointmentCreate: React.FC = () => {
             maxLength={100}
             numberOfLines={5}
             autoCorrect={false}
+            onChangeText={setDescription}
           />
           <View style={styles.footer}>
-            <Button title="Agendar" onPress={() => {}} />
+            <Button title="Agendar" onPress={handleSave} />
           </View>
         </View>
       </ScrollView>
