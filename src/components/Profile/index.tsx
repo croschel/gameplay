e42203157270e5581973d333748f722fb1d791ee
Caique Roschel <croschel000@gmail.com>
2021-06-27 17:47:41 -0300

    Profile using info from server

--- src/components/Profile/index.tsx
+++ src/components/Profile/index.tsx
@@ -1,16 +1,19 @@
 import React from "react";
 import { View, Text } from "react-native";
 import Avatar from "~/components/Avatar";
+import { useAuth } from "~/hooks/auth";
 import { styles } from "./styles";
 
 const Profile: React.FC = () => {
+  const { user } = useAuth();
+  console.log(user);
   return (
     <View style={styles.container}>
-      <Avatar urlImage="http://github.com/croschel.png" />
+      <Avatar urlImage={user.avatar} />
       <View>
         <View style={styles.user}>
           <Text style={styles.greeting}>Olá,</Text>
-          <Text style={styles.username}>Caique</Text>
+          <Text style={styles.username}>{user.firstName}</Text>
         </View>
         <Text style={styles.message}>Hoje é dia de vitória</Text>
       </View>
